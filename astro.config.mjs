42ba2cad2f5d0e7222f1307bf728d58a56968528
GLM <tubes.sake.0y@icloud.com>
2023-12-26 08:52:44 -0800

    1st
    1sst

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://astro.pages.github.io/',
+  base: '/site',
+  integrations: [mdx(), sitemap(), tailwind()]
+});
